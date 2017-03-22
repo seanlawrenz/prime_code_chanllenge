1b1092b3534f931a89022c8b0649e6b4aef2ff66
Sean Lawrenz <seanlaw27@gmail.com>
2017-03-21 21:37:51 -0400

    functionality done

--- test/test.js
+++ test/test.js
@@ -23,7 +23,7 @@ describe('isPrime', function() {
 
 describe('sortingArray', function(){
 	it('should sort numbers in an array numerically', function(){
-		var arr = [10,9,8,];
+		var arr = [10,9];
 		arr = arr.sort(function(a,b){
 			return a - b;
 		});
@@ -32,7 +32,45 @@ describe('sortingArray', function(){
 	});
 });
 
-//Stealing the Sieve of Eratosthenes algorithm
 describe('primeNumberGenerator', function(){
+	describe('printPrime', function(){
+		it('should find all prime numbers up to a limit', function(){
+			var isPrime =  function(number) {
+				for(var i = 2; i < number; i++) {
+					if(number % i === 0) {
+						return false;
+					}
+				}
+				return number > 1;	
+			}
+			var printPrime = function(input1, input2) {
+				var arr = [input1,input2];
+				arr = arr.sort(function(a,b){
+					return a - b;
+				});
+				var lowNumber = arr[0];
+				var highNumber = arr[1];
+				var primeNumber = []
+				for(var i = lowNumber; i < highNumber; i++) {
+					if (isPrime(i) === true){
+						primeNumber.push(i)
+					}
+				}
+				return primeNumber
+			}
 
+			expect(printPrime(7900, 7920)).to.deep.equal([ 7901, 7907, 7919 ]);
+		});
+	});
 });
+
+
+
+
+
+
+
+
+
+
+
